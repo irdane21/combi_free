89413450c41be72c0270bd2da9acd5bd43ec80be
Louis1404 <40355564+Louis1404@users.noreply.github.com>
2018-08-23 12:16:49 +0200

    Update autocomplete.js

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,9 +1,9 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var search = document.querySelector('.search');
+    const search = document.querySelector('.search');
 
     if (search) {
-      var autocomplete = new google.maps.places.Autocomplete(search, { types: [ 'geocode' ] });
+      const autocomplete = new google.maps.places.Autocomplete(search, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(search, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
