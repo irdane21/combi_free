c33c68f8d2869ddbd57a67604b400789c7dbaadf
louisburette <louis.burette@ieseg.fr>
2018-08-23 12:09:24 +0200

    searchform works with autocompletion

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,10 +1,10 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var combiAddress = document.getElementById('combi_address');
+    var search = document.querySelector('.search');
 
-    if (combiAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(combiAddress, { types: [ 'geocode' ] });
-      google.maps.event.addDomListener(combiAddress, 'keydown', function(e) {
+    if (search) {
+      var autocomplete = new google.maps.places.Autocomplete(search, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(search, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
         }
